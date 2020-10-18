406af1041109e283c4dfe5df04b6ed692fbf48bc
takroid <sheetake1204@gmail.com>
2020-10-18 15:26:01 +0900

    initial commit

---
+++ src/com/luv2code/springdemo/BoxingCoach.java
@@ -0,0 +1,11 @@
+package com.luv2code.springdemo;
+
+public class BoxingCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		// TODO 自動生成されたメソッド・スタブ
+		return "punch * 5hours";
+	}
+
+}
---
+++ src/com/luv2code/springdemo/TrackCoach.java
@@ -0,0 +1,11 @@
+package com.luv2code.springdemo;
+
+public class TrackCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		// TODO 自動生成されたメソッド・スタブ
+		return "Run a hard 5k";
+	}
+
+}
