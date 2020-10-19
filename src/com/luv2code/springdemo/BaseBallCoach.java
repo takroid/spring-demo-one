bb703f34ff4940acae0d276b65f64d958050afae
takroid <sheetake1204@gmail.com>
2020-10-19 22:23:20 +0900

    続き

--- src/com/luv2code/springdemo/BaseBallCoach.java
+++ src/com/luv2code/springdemo/BaseBallCoach.java
@@ -2,6 +2,14 @@ package com.luv2code.springdemo;
 
 public class BaseBallCoach implements Coach {
 
+	//依存性の為privateで宣言
+	private FortuneService fortuneService;
+
+	//依存性の注入のためコンストラクタ定義	
+	public BaseBallCoach(FortuneService theFortuneService) {
+		fortuneService = theFortuneService;
+	}
+
 	@Override
 	public String getDailyWorkout() {
 		return "Spend 30 minutes on batting practice";
@@ -9,7 +17,8 @@ public class BaseBallCoach implements Coach {
 
 	@Override
 	public String getDailyFortune() {
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
+
+		//use my fortuneService to get a Fortune
+		return fortuneService.getFortune();
 	}
 }
